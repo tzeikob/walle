8b7da78d706b8004d73f288bbd32708b858591fb
Jake Ob <iakopap@gmail.com>
2021-10-09 17:38:20 +0300

    Clean up autostart file after apt remove (refs #2)

--- src/hooks/postrm.sh
+++ src/hooks/postrm.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-# A script to be run after remove installation
+# A script to be run after apt remove
 
 set -e
 
 NAME=PKG_NAME
-CONFIG_DIR=/home/$SUDO_USER/.config/$NAME
+USER_HOME=/home/$SUDO_USER
+CONFIG_DIR=$USER_HOME/.config/$NAME
+AUTOSTART_DIR=$USER_HOME/.config/autostart
 
 echo -e "Startig post remove script"
 
-echo -e "Deleting config and utility files in '$CONFIG_DIR'"
-
-# Delete config and utility files
+# Delete config folder
 rm -rf $CONFIG_DIR
 
-echo -e "Config files have been removed"
+echo -e "Config folder has been removed"
+
+# Remove autostart desktop file
+rm -f $AUTOSTART_DIR/$NAME.desktop
+
+echo -e "Autostart desktop file has been removed"
 
 echo -e "Exiting post remove script"
 
