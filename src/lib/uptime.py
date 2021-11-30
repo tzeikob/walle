bfecb8fbf18cb874459e7392fb6adf4b95e291b0
Jake Ob <iakopap@gmail.com>
2021-11-30 14:13:14 +0200

    Catch and handle raised exceptions per resolving call
    We better organized the code in order to catch and handle failed
    resolving calls in the way to not block and swallowing calls following
    a failed resolve call. Each failing call should only report the error
    and return a nullish value back to its caller.
    
    Moreover with this change we managed to enforce the public API each
    resolve module should implement.

--- src/lib/uptime.py
+++ src/lib/uptime.py
@@ -6,21 +6,22 @@ import math
 
 # Load native c libraries
 libc = ctypes.CDLL('libc.so.6')
-
-# Initialize the buffer
 buf = ctypes.create_string_buffer(4096)
 
 # Returns the current uptime in hours, mins and secs
 def resolve ():
-  try:
-    if libc.sysinfo(buf) == 0:
-      secs = struct.unpack_from('@l', buf.raw)[0]
-    else:
-      # Otherwise try to read the proc file
-      with open('/proc/uptime') as uptime_file:
-        secs = float(uptime_file.readline().split()[0])
-  except Exception:
-    return None
+  if libc.sysinfo(buf) == 0:
+    secs = struct.unpack_from('@l', buf.raw)[0]
+  else:
+    # Otherwise fallback to the proc file
+    with open('/proc/uptime') as uptime_file:
+      secs = float(uptime_file.readline().split()[0])
+  
+  if not isinstance(secs, (int, float)):
+    raise Exception(f'uptime resolved to a non numeric value: {secs}')
+
+  if secs < 0:
+    raise Exception(f'uptime resolved to a negative value: {secs}')
 
   # Calculate how many hours
   hours = math.floor (secs / 3600)
--- src/resolver.py
+++ src/resolver.py
@@ -8,22 +8,28 @@ import globals
 import logger
 from lib import uptime
 
+# Executes the resolve API of the given callback
+def run (callback):
+  result = None
+
+  try:
+    # Call the resolve method each cb should has
+    result = callback.resolve()
+  except Exception as error:
+    # Just report and return none
+    logger.disk.trace(error)
+
+  return result
+
 data = {}
 
 while True:
-  try:
-    logger.disk.info('gathering system data...')
+  logger.disk.info('gathering system data...')
 
-    data['uptime'] = uptime.resolve()
-    data['last'] = str(datetime.now())
+  data['uptime'] = run(uptime)
+  data['last'] = str(datetime.now())
 
-    logger.disk.info('system data have been resolved')
-  except Exception as error:
-    # Just report and skip to the next cycle
-    logger.disk.trace(error)
-
-    time.sleep(globals.RESOLVER_INTERVAL)
-    continue
+  logger.disk.info('system data have been resolved')
 
   # Write down the collected data to the disk
   with open(globals.DATA_FILE_PATH, 'w') as data_file:
