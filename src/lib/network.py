f32a9b75473fcf453333f317e3c544ea59b42bc2
Jake Ob <iakopap@gmail.com>
2021-12-15 18:02:18 +0200

    Report zero net speeds for invalid network counters

--- src/lib/network.py
+++ src/lib/network.py
@@ -9,8 +9,8 @@ from convert import text, integer, decimal, MB, Mb
 data = {}
 
 # Sent and recv bytes since the last call
-last_sent = None
-last_recv = None
+last_sent = 0
+last_recv = 0
 
 # Last date time since the last call
 last = datetime.now()
@@ -61,16 +61,20 @@ def resolve ():
   # Calculate current download and upload speeds
   global last_sent, last_recv
 
-  if not last_sent and not last_recv:
+  # Report zero up speed in case of invalid sent value
+  if last_sent >= sent or past_secs <= 0:
     up_speed = 0
-    down_speed = 0
   else:
     delta = sent - last_sent
     up_speed = delta / past_secs
 
+  # Report zero down speed in case of invalid recv value
+  if last_recv >= recv or past_secs <= 0:
+    down_speed = 0
+  else:
     delta = recv - last_recv
     down_speed = delta / past_secs
-  
+
   # Save last sent and recv bytes for the next call
   last_sent = sent
   last_recv = recv
