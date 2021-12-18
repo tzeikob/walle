070256e846df9d35886d1a50823c690e289e4217
Jake Ob <iakopap@gmail.com>
2021-12-18 20:21:34 +0200

    Rename resolver modules to module tasks for readability

--- src/resolver.py
+++ src/resolver.py
@@ -15,27 +15,27 @@ import time
 import globals
 import config
 from logger import Router
-from lib import release
-from lib import login
-from lib import hardware
-from lib import loads
-from lib import thermals
-from lib import network
+from tasks import release
+from tasks import login
+from tasks import hardware
+from tasks import loads
+from tasks import thermals
+from tasks import network
 
 # Marks process as not up and running on kill signals
 def mark_shutdown (*args):
   global is_up
   is_up = False
 
-# Executes the resolve API of the given callback
-def run (module):
+# Resolve the given module task with fallback protection
+def resolve (task):
   result = None
 
   try:
-    # Call the resolve method each module should has
-    result = module.resolve()
+    # Call the resolve method each task should has
+    result = task.resolve()
   except Exception as exc:
-    # Just report and return none
+    # Just report and fallback to return none
     logger.disk.trace(exc)
 
   return result
@@ -58,7 +58,7 @@ signal.signal(signal.SIGTERM, mark_shutdown)
 
 logger.disk.debug(f"resolving hardware data at {time.strftime(globals.TIME_FORMAT)}")
 
-hardware_data = run(hardware)
+hardware_data = resolve(hardware)
 
 # Read memory data already resolved at installation
 with open(globals.DATA_DIR_PATH + '/hardware') as hardware_file:
@@ -71,7 +71,7 @@ with open(globals.DATA_DIR_PATH + '/hardware', 'w') as hardware_file:
 
 logger.disk.debug(f'resolving release data at {time.strftime(globals.TIME_FORMAT)}')
 
-release_data = run(release)
+release_data = resolve(release)
 
 logger.disk.debug(f'release data resolved:\n{release_data}')
 
@@ -80,7 +80,7 @@ with open(globals.DATA_DIR_PATH + '/release', 'w') as release_file:
 
 logger.disk.debug(f'resolving login data at {time.strftime(globals.TIME_FORMAT)}')
 
-login_data = run(login)
+login_data = resolve(login)
 
 logger.disk.debug(f'login data resolved:\n{login_data}')
 
@@ -93,19 +93,19 @@ while is_up:
 
   logger.disk.debug(f'resolving loads data at {time.strftime(globals.TIME_FORMAT)}')
 
-  monitor_data['loads'] = run(loads)
+  monitor_data['loads'] = resolve(loads)
 
   logger.disk.debug(f"loads data resolved:\n{monitor_data['loads']}")
 
   logger.disk.debug(f'resolving thermals data at {time.strftime(globals.TIME_FORMAT)}')
 
-  monitor_data['thermals'] = run(thermals)
+  monitor_data['thermals'] = resolve(thermals)
 
   logger.disk.debug(f"thermals data resolved:\n{monitor_data['thermals']}")
 
   logger.disk.debug(f'resolving network data at {time.strftime(globals.TIME_FORMAT)}')
 
-  monitor_data['network'] = run(network)
+  monitor_data['network'] = resolve(network)
 
   logger.disk.debug(f"network data resolved:\n{monitor_data['network']}")
 
