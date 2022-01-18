4be9f92985dae5b85d5d96f8ea964e8dc95c4176
Jake Ob <iakopap@gmail.com>
2022-01-18 19:43:37 +0200

    Refactor conky text to be rendered with cairo as ui modules
    * conky text lines are now rendering via cairo
    * each conky text line has been modeled into a ui component
    * layout has been changed to top single row orientation
    * some of the lines have been droped to keep the minimal style

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -123,199 +123,52 @@ function conky_draw ()
     ui.render_grid ()
   end
 
+  -- Read context vars
+  local vars = context.vars
+
+  -- Attach processor ui component
+  local cpu_clock = util.opt (vars["cpu_clock"], 0)
+  local cpu_util = util.opt (vars["cpu_util"], 0.0)
+
+  cpu_clock = format.int (cpu_clock, "%04d") .. "MHz"
+  cpu_util = format.int (cpu_util, "%02d") .. "%"
+
+  ui.attach ("PROCESSOR", cpu_clock, cpu_util)
+
+  -- Attach graphics ui component
+  local gpu_used = util.opt (vars["gpu_used"], 0)
+  local gpu_util = util.opt (vars["gpu_util"], 0.0)
+
+  gpu_used = format.int (gpu_used, "%05d") .. "MB"
+  gpu_util = format.int (gpu_util, "%02d") .. "%"
+
+  ui.attach ("GRAPHICS", gpu_used, gpu_util)
+
+  -- Attach memory ui component
+  local mem_used = util.opt (vars["mem_used"], 0)
+  local mem_util = util.opt (vars["mem_util"], 0.0)
+
+  mem_used = format.int (mem_used, "%06d") .. "MB"
+  mem_util = format.int (mem_util, "%02d") .. "%"
+
+  ui.attach ("MEMORY", mem_used, mem_util)
+
+  -- Attach root disk ui component
+  local disk_root_used = util.opt (vars["disk_root_used"], 0)
+  local disk_root_util = util.opt (vars["disk_root_util"], 0.0)
+  local disk_root_type = util.opt (vars["disk_root_type"])
+
+  disk_root_used = format.int (disk_root_used, "%06d") .. "MB"
+  disk_root_util = format.int (disk_root_util, "%02d") .. "%"
+  disk_root_type = format.upper (disk_root_type)
+
+  ui.attach ("DISK " .. disk_root_type, disk_root_used, disk_root_util)
+
+  -- Render ui into the canvas
+  ui.render ()
+
   -- Destroy ui context
   ui.destroy ()
 
   logger.debug ("exiting the post conky draw phase")
-end
-
--- Converts the given text to a conkyrc text line
-function ln (text, scale)
-  -- Read the interpolation variables from the context
-  local vars = context.vars
-
-  local line = "${alignr}"
-
-  line = line .. "${font " .. vars["font_name"]
-
-  if vars["font_bold"] then
-    line = line .. ":bold"
-  end
-
-  if vars["font_italic"] then
-    line = line .. ":italic"
-  end
-
-  local size = vars["font_size"]
-
-  if scale and scale > 1 then
-    size = size * scale
-  end
-
-  line = line .. ":size=" .. size .. "}" .. text .. "${font}" .. "\n"
-
-  return line
-end
-
--- Builds and returns the conky text
-function conky_text ()
-  -- Read the interpolation variables from the context
-  local vars = context.vars
-
-  local text = ""
-
-  -- Add font color theme
-  local theme_color = vars["theme_color"]
-
-  text = text .. "${color " .. theme_color .. "}\n"
-
-  -- Build the optional head line text
-  local head_line = vars["head_line"]
-
-  if util.given (head_line) then
-    head_line = format.upper (head_line)
-
-    text = text .. ln (head_line, 1.4)
-  end
-
-  -- Build the user and host line text
-  local user = util.opt (vars["user"])
-  local host = util.opt (vars["host"])
-
-  user = format.upper (user)
-  host = format.upper (host)
-
-  text = text .. ln ("USER " .. user .. " HOST " .. host)
-
-  -- Build the release line text
-  local rls_name = util.opt (vars["rls_name"])
-  local rls_codename = util.opt (vars["rls_codename"])
-  local rls_version = util.opt (vars["rls_version"])
-
-  rls_name = format.upper (rls_name)
-  rls_codename = format.upper (rls_codename)
-  rls_version = format.upper (rls_version)
-
-  text = text .. ln ("SYSTEM " .. rls_name .. " " .. rls_codename .. " v" .. rls_version) .. "\n"
-
-  -- Build the cpu line text
-  local cpu_util = util.opt (vars["cpu_util"], 0.0)
-  local cpu_clock = util.opt (vars["cpu_clock"], 0)
-  local cpu_temp = util.opt (vars["cpu_temp"], 0.0)
-
-  cpu_util = format.int (cpu_util, "%02d")
-  cpu_clock = format.int (cpu_clock, "%04d")
-  cpu_temp = format.int (cpu_temp, "%02d")
-
-  text = text .. ln ("PROCESSOR " .. cpu_util .. "% " .. cpu_clock .. "MHz " .. cpu_temp .. "°C")
-
-    -- Build the gpu line text
-  local gpu_util = util.opt (vars["gpu_util"], 0.0)
-  local gpu_used = util.opt (vars["gpu_used"], 0)
-  local gpu_temp = util.opt (vars["gpu_temp"], 0.0)
-
-  gpu_util = format.int (gpu_util, "%02d")
-  gpu_used = format.int (gpu_used, "%05d")
-  gpu_temp = format.int (gpu_temp, "%02d")
-
-  text = text .. ln ("GRAPHICS " .. gpu_util .. "% " .. gpu_used .. "MB " .. gpu_temp .. "°C")
-
-  -- Build the memory line text
-  local mem_util = util.opt (vars["mem_util"], 0.0)
-  local mem_used = util.opt (vars["mem_used"], 0)
-  local mem_speed = util.opt (vars["mem_speed"], 0)
-
-  mem_util = format.int (mem_util, "%02d")
-  mem_used = format.int (mem_used, "%06d")
-  mem_speed = format.int (mem_speed, "%04d")
-
-  text = text .. ln ("MEMORY " .. mem_util .. "% " .. mem_used .. "MB " .. mem_speed .. "MHz")
-
-  -- Build the disk line text
-  local disk_root_util = util.opt (vars["disk_root_util"], 0.0)
-  local disk_root_used = util.opt (vars["disk_root_used"], 0)
-  local disk_root_free = util.opt (vars["disk_root_free"], 0)
-  local disk_root_type = util.opt (vars["disk_root_type"])
-
-  disk_root_util = format.int (disk_root_util, "%02d")
-  disk_root_used = format.int (disk_root_used, "%06d")
-  disk_root_free = format.int (disk_root_free, "%06d")
-  disk_root_type = format.upper (disk_root_type)
-
-  text = text .. ln ("DISK ROOT " .. disk_root_type .. " " .. disk_root_util .. "% Ux " .. disk_root_used .. "MB Fx " .. disk_root_free .. "MB")
-
-  if util.given(vars["disk_home_util"]) then
-    local disk_home_util = util.opt (vars["disk_home_util"], 0.0)
-    local disk_home_used = util.opt (vars["disk_home_used"], 0)
-    local disk_home_free = util.opt (vars["disk_home_free"], 0)
-    local disk_home_type = util.opt (vars["disk_home_type"])
-  
-    disk_home_util = format.int (disk_home_util, "%02d")
-    disk_home_used = format.int (disk_home_used, "%06d")
-    disk_home_free = format.int (disk_home_free, "%06d")
-    disk_home_type = format.upper (disk_home_type)
-
-    text = text .. ln ("DISK HOME " .. disk_home_type .. " " .. disk_home_util .. "% Ux " .. disk_home_used .. "MB Fx " .. disk_home_free .. "MB")
-  end
-
-  local disk_read = util.opt (vars["disk_read"], 0)
-  local disk_read_speed = util.opt (vars["disk_read_speed"], 0.0)
-
-  local disk_write = util.opt (vars["disk_write"], 0)
-  local disk_write_speed = util.opt (vars["disk_write_speed"], 0.0)
-
-  disk_read = format.int (disk_read, "%05d")
-  disk_read_speed = format.number (disk_read_speed, "%06.1f")
-
-  disk_write = format.int (disk_write, "%05d")
-  disk_write_speed = format.number (disk_write_speed, "%06.1f")
-
-  text = text .. ln ("READ " .. disk_read .. "MB " .. disk_read_speed .. "MB/s")
-  text = text .. ln ("WRITE " .. disk_write .. "MB " .. disk_write_speed .. "MB/s")
-
-  -- Build the upload network line text
-  local net_sent_bytes = 0
-  local net_up_speed = 0.0
-
-  if vars["net_up"] then
-    net_sent_bytes = util.opt (vars["net_sent_bytes"], 0)
-    net_up_speed = util.opt (vars["net_up_speed"], 0.0)
-  end
-
-  net_sent_bytes = format.int (net_sent_bytes, "%05d")
-  net_up_speed = format.number (net_up_speed, "%05.1f")
-
-  text = text .. ln (" UPLOAD " .. net_sent_bytes .. "MB " .. net_up_speed .. "Mbps")
-
-  -- Build the download network line text
-  local net_recv_bytes = 0
-  local net_down_speed = 0.0
-
-  if vars["net_up"] then
-    net_recv_bytes = util.opt (vars["net_recv_bytes"], 0)
-    net_down_speed = util.opt (vars["net_down_speed"], 0.0)
-  end
-
-  net_recv_bytes = format.int (net_recv_bytes, "%05d")
-  net_down_speed = format.number (net_down_speed, "%05.1f")
-
-  text = text .. ln ("DOWNLOAD " .. net_recv_bytes .. "MB " .. net_down_speed .. "Mbps")
-
-  -- Build the local network line text
-  local net_name = "NA"
-  local lan_ip = "x.x.x.x"
-
-  if vars["net_up"] then
-    net_name = format.upper (util.opt (vars["net_name"]))
-    lan_ip = util.opt (vars["lan_ip"], "x.x.x.x")
-  end
-
-  text = text .. ln ("NETWORK " .. net_name .. " IP " .. lan_ip) .. "\n"
-
-  -- Build the uptime line text
-  local uptime = util.opt (vars["uptime"])
-
-  text = text .. ln ("UPTIME T+" .. uptime)
-
-  return conky_parse (text)
 end
--- src/lua/ui.lua
+++ src/lua/ui.lua
@@ -12,10 +12,14 @@ local right = 0
 local viewport = {
   canvas = nil,
   surface = nil,
+  extents = nil,
   scale = 1
 }
 
--- Initializes the drawing viewport given the conky window
+-- List of the ui components to render
+local components = {}
+
+-- Initializes the viewport given the conky window
 function init (window, scale, offsets)
   -- Read conky window properties
   local display = window.display
@@ -29,6 +33,10 @@ function init (window, scale, offsets)
 
   -- Create the drawing context object
   viewport.canvas = cairo_create (viewport.surface)
+
+  -- Initialize the processor to resolve text size in pixels
+  viewport.extents = cairo_text_extents_t:create()
+  tolua.takeownership(viewport.extents)
 
   -- Set the scaling factor
   viewport.scale = scale
@@ -165,7 +173,125 @@ function render_grid ()
   end
 end
 
--- Destroys the viewport
+-- Resolves the given text as a text ui component
+function resolve_text (text, size, red, green, blue, alpha)
+  if text == nil or text == "" then
+    return nil
+  end
+
+  local canvas = viewport.canvas
+  local extents = viewport.extents
+
+  local font = {
+    name = "Ubuntu Mono",
+    slant = CAIRO_FONT_SLANT_ITALIC,
+    face = CAIRO_FONT_WEIGHT_BOLD,
+    size = size * viewport.scale,
+    red = red,
+    green = green,
+    blue = blue,
+    alpha = alpha
+  }
+
+  -- Set font and styles
+  cairo_select_font_face (canvas, font.name, font.slant, font.face)
+  cairo_set_font_size (canvas, font.size)
+
+  -- Resolve the size the text will take in canvas
+  cairo_text_extents (canvas, text, extents)
+
+  return {
+    value = text,
+    font = font,
+    width = extents.width,
+    height = extents.height
+  }
+end
+
+-- Register the given values as a concrete ui component
+function attach (label, val1, val2)
+  -- Resolve any given argument into its text ui component
+  label = resolve_text (label, 30, 1, 1, 1, 1)
+  val1 = resolve_text (val1, 24, 1, 1, 1, 1)
+  val2 = resolve_text (val2, 24, 1, 1, 1, 1)
+
+  -- Compute the total width and height of the component
+  local padding = 6 * viewport.scale
+
+  -- Set as the width the lenghtiest line
+  local width = label.width
+
+  if width < val1.width + padding + val2.width then
+    width = val1.width + padding + val2.width
+  end
+
+  local height = label.height + padding + val1.height
+
+  -- Encapsulate the values into a concrete component
+  local component = {
+    label = label,
+    val1 = val1,
+    val2 = val2,
+    val3 = val3,
+    width = width,
+    height = height
+  }
+
+  -- Add the component into the rendering list
+  table.insert (components, component)
+end
+
+-- Renders the given text component at the given location in canvas
+function draw_text (text, x, y)
+  local canvas = viewport.canvas
+
+  -- Set text font and styles
+  local font = text.font
+
+  cairo_select_font_face (canvas, font.name, font.slant, font.face)
+  cairo_set_font_size (canvas, font.size)
+  cairo_set_source_rgba (canvas, font.red, font.green, font.blue, font.alpha)
+
+  -- Draw text at the location in canvas
+  cairo_move_to (canvas, x, y)
+  cairo_show_text (canvas, text.value)
+  cairo_stroke (canvas)
+end
+
+-- Renders all the ui components
+function render ()
+  local scale = viewport.scale
+
+  local offset = 30 * scale
+  local padding = 6 * scale
+
+  local x = right - offset
+  local y = top + offset
+
+  for i, component in ipairs (components) do
+    x = x - component.width
+    y = y + component.height
+
+    draw_text (component.label, x, y)
+
+    x = x + 4
+    y = y - component.label.height - padding
+
+    draw_text (component.val1, x, y)
+
+    x = x + component.val1.width + padding
+
+    draw_text (component.val2, x, y)
+
+    x = x - 4 - component.val1.width - padding - offset
+    y = y + component.label.height + padding - component.height
+  end
+
+  -- Clear the rendering list
+  components = {}
+end
+
+-- Destroys the viewport references
 function destroy ()
   cairo_destroy (viewport.canvas)
   cairo_surface_destroy (viewport.surface)
@@ -173,11 +299,14 @@ function destroy ()
   -- Release object references from memory
   viewport.canvas = nil
   viewport.surface = nil
+  viewport.extents = nil
 end
 
 return {
   init = init,
   destroy = destroy,
   render_borders = render_borders,
-  render_grid = render_grid
+  render_grid = render_grid,
+  attach = attach,
+  render = render
 }
