99a485980d96aff7c35b191144a55ee5e5d57c6b
Jake Ob <iakopap@gmail.com>
2022-03-07 20:42:25 +0200

    Drop the old school conky components

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -17,7 +17,6 @@ package.path = package.path .. ";" .. BASE_DIR .. "/?.lua;" .. BASE_DIR .. "/com
 local ui = require "ui"
 local util = require "util"
 local logging = require "logging"
-local format = require "format"
 
 -- create logger and set it in global scope
 logger = logging.Logger:new (LOG_FILE_PATH)
@@ -69,58 +68,7 @@ function conky_draw ()
   -- Initialize ui context
   ui.init (conky_window, dark, scale, offsets)
 
-  -- Attach processor ui component
-  local cpu_clock = util.opt (data["monitor"]["cpu"]["clock"], 0)
-  local cpu_util = util.opt (data["monitor"]["cpu"]["util"], 0.0)
-
-  cpu_clock = format.int (cpu_clock, "%04d") .. "MHz"
-  cpu_util = format.int (cpu_util, "%02d") .. "%"
-
-  ui.attach ("PROCESSOR", cpu_clock, cpu_util)
-
-  -- Attach graphics ui component
-  local gpu_used = util.opt (data["monitor"]["gpu"]["used"], 0)
-  local gpu_util = util.opt (data["monitor"]["gpu"]["util"], 0.0)
-
-  gpu_used = format.int (gpu_used, "%05d") .. "MB"
-  gpu_util = format.int (gpu_util, "%02d") .. "%"
-
-  ui.attach ("GRAPHICS", gpu_used, gpu_util)
-
-  -- Attach memory ui component
-  local mem_used = util.opt (data["monitor"]["memory"]["used"], 0)
-  local mem_util = util.opt (data["monitor"]["memory"]["util"], 0.0)
-
-  mem_used = format.int (mem_used, "%06d") .. "MB"
-  mem_util = format.int (mem_util, "%02d") .. "%"
-
-  ui.attach ("MEMORY", mem_used, mem_util)
-
-  -- Attach root disk ui component
-  local disk_root_used = util.opt (data["monitor"]["disk"]["used"], 0)
-  local disk_root_util = util.opt (data["monitor"]["disk"]["util"], 0.0)
-
-  disk_root_used = format.int (disk_root_used, "%06d") .. "MB"
-  disk_root_util = format.int (disk_root_util, "%02d") .. "%"
-
-  ui.attach ("DISK " .. 'EXT4', disk_root_used, disk_root_util)
-
-  -- Attach keyboard ui component
-  local kb_press = util.opt (data["keyboard"]["press"], 0)
-  kb_press = format.int (kb_press, "%06d")
-
-  ui.attach ("KEYBOARD", kb_press, "Keys")
-
-  -- Attach mouse clicks ui component
-  local ms_clicks = util.opt (data["mouse"]["left"], 0) + util.opt (data["mouse"]["right"], 0) + util.opt (data["mouse"]["middle"], 0)
-  ms_clicks = format.int (ms_clicks, "%06d")
-
-  ui.attach ("MOUSE", ms_clicks, "Clks")
-
-  -- Attach the uptime component
-  ui.attach ("UPTIME", "T+" .. data["uptime"]["hours"] .. ":" .. data["uptime"]["mins"] .. ":" .. data["uptime"]["secs"], "-")
-
-  -- Render ui into the canvas
+  -- Render ui components
   ui.render ()
 
   -- Destroy ui context
--- src/lua/ui.lua
+++ src/lua/ui.lua
@@ -16,9 +16,6 @@ viewport = {
   scale = 1
 }
 
--- List of the ui components to render
-local components = {}
-
 -- Initializes the viewport given the conky window
 function init (window, dark, scale, offsets)
   -- Read conky window properties
@@ -35,8 +32,8 @@ function init (window, dark, scale, offsets)
   viewport.canvas = cairo_create (viewport.surface)
 
   -- Initialize the processor to resolve text size in pixels
-  viewport.extents = cairo_text_extents_t:create()
-  tolua.takeownership(viewport.extents)
+  viewport.extents = cairo_text_extents_t:create ()
+  tolua.takeownership (viewport.extents)
 
   -- Set dark mode
   viewport.dark = dark
@@ -60,129 +57,10 @@ function init (window, dark, scale, offsets)
   viewport.right = viewport.right + offsets.right
 end
 
--- Resolves the given text as a text ui component
-function resolve_text (text, size)
-  if text == nil or text == "" then
-    return nil
-  end
-
-  local canvas = viewport.canvas
-  local extents = viewport.extents
-
-  local font = {
-    name = "Ubuntu Mono",
-    slant = CAIRO_FONT_SLANT_ITALIC,
-    face = CAIRO_FONT_WEIGHT_BOLD,
-    size = size * viewport.scale
-  }
-
-  -- Set font and styles
-  cairo_select_font_face (canvas, font.name, font.slant, font.face)
-  cairo_set_font_size (canvas, font.size)
-
-  -- Resolve the size the text will take in canvas
-  cairo_text_extents (canvas, text, extents)
-
-  return {
-    value = text,
-    font = font,
-    width = extents.width,
-    height = extents.height
-  }
-end
-
--- Register the given values as a concrete ui component
-function attach (label, val1, val2)
-  -- Resolve any given argument into its text ui component
-  label = resolve_text (label, 30)
-  val1 = resolve_text (val1, 24)
-  val2 = resolve_text (val2, 24)
-
-  -- Compute the total width and height of the component
-  local padding = 6 * viewport.scale
-
-  -- Set as the width the lenghtiest line
-  local width = label.width
-
-  if width < val1.width + padding + val2.width then
-    width = val1.width + padding + val2.width
-  end
-
-  local height = label.height + padding + val1.height
-
-  -- Encapsulate the values into a concrete component
-  local component = {
-    label = label,
-    val1 = val1,
-    val2 = val2,
-    val3 = val3,
-    width = width,
-    height = height
-  }
-
-  -- Add the component into the rendering list
-  table.insert (components, component)
-end
-
--- Renders the given text component at the given location in canvas
-function draw_text (text, x, y)
-  local canvas = viewport.canvas
-
-  -- Set text font and styles
-  local font = text.font
-
-  cairo_select_font_face (canvas, font.name, font.slant, font.face)
-  cairo_set_font_size (canvas, font.size)
-
-  -- Set font color
-  local r, g, b = 1, 1, 1
-
-  if viewport.dark then
-    r, g, b = 0, 0, 0
-  end
-
-  cairo_set_source_rgba (canvas, r, g, b, 1)
-
-  -- Draw text at the location in canvas
-  cairo_move_to (canvas, x, y)
-  cairo_show_text (canvas, text.value)
-  cairo_stroke (canvas)
-end
-
 -- Renders all the ui components
 function render ()
   local grid = grid.Grid:new ({ 1, 1, 1, 0.8 })
   grid:render ()
-
-  local scale = viewport.scale
-
-  local offset = 30 * scale
-  local padding = 6 * scale
-
-  local x = viewport.right - offset
-  local y = viewport.top + offset
-
-  for i, component in ipairs (components) do
-    x = x - component.width
-    y = y + component.height
-
-    draw_text (component.label, x, y)
-
-    x = x + 4
-    y = y - component.label.height - padding
-
-    draw_text (component.val1, x, y)
-
-    x = x + component.val1.width + padding
-
-    draw_text (component.val2, x, y)
-
-    x = x - 4 - component.val1.width - padding - offset
-    y = y + component.label.height + padding - component.height
-  end
-
-  -- Clear the rendering list
-  components = {}
 end
 
 -- Destroys the viewport references
@@ -198,7 +76,6 @@ end
 
 return {
   init = init,
-  destroy = destroy,
-  attach = attach,
-  render = render
+  render = render,
+  destroy = destroy
 }
