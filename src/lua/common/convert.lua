c014d6256c3c0bdf6ee20439cb1fd9eab20eec1e
Jake Ob <iakopap@gmail.com>
2022-03-08 18:52:11 +0200

    Have functions local to keep lua global scope clean

--- src/lua/common/convert.lua
+++ src/lua/common/convert.lua
@@ -1,44 +1,44 @@
 -- A lua module to convert between different units
 
 -- Rounds the given value to the given number of decimal places
-function round (value, precision)
+local function round (value, precision)
   local mult = 10^(precision or 0)
 
   return math.floor(value * mult + 0.5) / mult
 end
 
 -- Converts bytes to bits
-function b (bytes)
+local function b (bytes)
   return bytes * 8
 end
 
 -- Converts bytes to kilo bits
-function Kb (bytes)
+local function Kb (bytes)
   return b(bytes) / 1024
 end
 
 -- Converts bytes to mega bits
-function Mb (bytes)
+local function Mb (bytes)
   return b(bytes) / (1024 * 1024)
 end
 
 -- Converts bytes to giga bits
-function Gb (bytes)
+local function Gb (bytes)
   return b(bytes) / (1024 * 1024 * 1024)
 end
 
 -- Converts bytes to kilo bytes
-function KB (bytes)
+local function KB (bytes)
   return bytes / 1024
 end
 
 -- Converts bytes to mega bytes
-function MB (bytes)
+local function MB (bytes)
   return bytes / (1024 * 1024)
 end
 
 -- Converts bytes to giga bytes
-function GB (bytes)
+local function GB (bytes)
   return bytes / (1024 * 1024 * 1024)
 end
 
