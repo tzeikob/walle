374723a91ba254849807aa7ce748fc36252d9aba
Jake Ob <iakopap@gmail.com>
2022-03-18 14:43:09 +0200

    Improve organization in the block scalar comp
    Organizing the internal properties of the component we can get a
    cleaner design making easier both development and maintainance.

--- src/lua/ui/block-scalar.lua
+++ src/lua/ui/block-scalar.lua
@@ -4,14 +4,18 @@ local convert = require "convert"
 
 local BlockScalar = {
   canvas = nil,
-  value = nil,
-  max = nil,
-  blocks = 12,
-  padding = 2,
-  dim = { 1, 1, 1, 0.4 },
-  normal = { 1, 1, 1, 0.8 },
-  low = { 0.8, 0, 0.1, 0.8 },
-  high = { 1, 0.9, 0, 0.8 },
+  data = {
+    value = 0,
+    max = 100
+  },
+  style = {
+    blocks = 12,
+    padding = 2,
+    dim = { 1, 1, 1, 0.4 },
+    normal = { 1, 1, 1, 0.8 },
+    low = { 0.8, 0, 0.1, 0.8 },
+    high = { 1, 0.9, 0, 0.8 },
+  },
   x = 0,
   y = 0,
   width = 0,
@@ -23,10 +27,10 @@ function BlockScalar:new (canvas, value, max, width, height)
   self.__index = self
 
   o.canvas = canvas
-  o.value = value
-  o.max = max
+  o.data.value = value
+  o.data.max = max
 
-  o.padding = o.padding * o.canvas.scale
+  o.style.padding = o.style.padding * o.canvas.scale
 
   o.x = 0
   o.y = 0
@@ -43,35 +47,35 @@ end
 
 function BlockScalar:render ()
   -- Reduce current value to an integer scalar of blocks
-  local ratio = self.value / self.max
-  local scalar = convert.round (ratio * self.blocks)
+  local ratio = self.data.value / self.data.max
+  local scalar = convert.round (ratio * self.style.blocks)
 
   -- Calculate the width each block should occupate
-  local padding_space = (self.blocks - 1) * self.padding
-  local block_width = (self.width - padding_space) / self.blocks
+  local padding_space = (self.style.blocks - 1) * self.style.padding
+  local block_width = (self.width - padding_space) / self.style.blocks
 
-  for block = 1, self.blocks, 1 do
-    local color = self.dim
+  for block = 1, self.style.blocks, 1 do
+    local color = self.style.dim
 
     -- Color only blocks rated lower than current scalar
     if block <= scalar then
       if block <= 2 then
         -- Use low color for the first 2 blocks at low scalar
         if scalar <= 2 then
-          color = self.low
+          color = self.style.low
         else
-          color = self.normal
+          color = self.style.normal
         end
       elseif block >= 3 and block <= 9 then
-        color = self.normal
+        color = self.style.normal
       else
         -- Use high color for the last 3 blocks
-        color = self.high
+        color = self.style.high
       end
     end
 
     -- Make sure no padding space is drawn for the first block
-    local x = self.x + (block_width + self.padding) * (block - 1)
+    local x = self.x + (block_width + self.style.padding) * (block - 1)
 
     self.canvas:draw_rectangle (x, self.y, block_width, self.height, color)
   end
